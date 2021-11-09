b36138e92a627a6d1bfb0022d0e28c4f4d93e6af
Zl0bster <s00per@ya.ru>
2021-11-09 23:11:11 +0300

    Tasks 1, 2 are done

--- LAB5/other.cpp
+++ LAB5/other.cpp
@@ -2,9 +2,38 @@
 
 ///////////////////////////////////////////////////
 
+int ref = 5;
 
+int square(int& x)
+{
+	return x*x;
+}
 
+int IncByValue(int x)
+{
+	return x + 1;
+}
 
+int IncByPointer(const int* x)
+{
+	return *x + 1;
+}
 
+int IncByReference(const int& x)
+{
+	return x + 1;
+}
 
+void Swap_p(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
 
+void Swap_r(int& x, int& y)
+{
+	int temp = x;
+	x = y;
+	y = temp;
+}
--- LAB5/other.h
+++ LAB5/other.h
@@ -1,3 +1,12 @@
 //Прототипы используемых в данном задании функций:
 
+extern int ref; 
 
+int square(int& x);
+
+int IncByValue(int x);
+int IncByPointer(const int* x);
+int IncByReference(const int& x);
+
+void Swap_p(int* x, int* y);
+void Swap_r(int& x, int& y);
