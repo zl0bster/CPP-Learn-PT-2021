cc173baa9f0e9a52dce90cb35cdf6647063332a6
zl0bster <45553832+zl0bster@users.noreply.github.com>
2021-12-06 21:21:45 +0300

    Update Lab1_stuff.h
    Task 4

--- C110_LAB1/Lab1_stuff.h
+++ C110_LAB1/Lab1_stuff.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <iostream>
 
+const size_t chArLineLen = 40;
+const size_t chArLineNum = 20;
+
 void print_array(const int* arr, size_t nLayers, size_t nLines, size_t nCols);
 // prints only 3d arrays
 int summ_array(const int* arr, size_t nLayers, size_t nLines, size_t nCols);
+void swap_pointers(char** p1, char** p2);
+void Swap_p(int* x, int* y);
+void print_ch_array(char charArray[][chArLineLen]);
