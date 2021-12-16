053f2036afba65bf068836a60a6cc6eefb79a977
zl0bster <45553832+zl0bster@users.noreply.github.com>
2021-12-16 19:21:59 +0300

    array using improved

--- C110_LAB2/LAB2_arr.h
+++ C110_LAB2/LAB2_arr.h
@@ -1,10 +1,17 @@
 #pragma once
-void init_arr();
-void close_arr();
-size_t get_size();
-bool add_item(int val);
-int get_item(size_t pos);
-bool is_val_present(int val);
-size_t get_metrics();
 
-void print_metrics();
+struct ArrData {
+	size_t arrSize;
+	size_t membersN;
+	int* arrPtr = nullptr;
+	size_t incStep;
+};
+
+ArrData* new_arr();
+void close_arr(ArrData* arr);
+size_t get_size(ArrData* arr);
+int get_item(ArrData* arr, size_t pos);
+bool is_val_present(ArrData* arr, int val);
+size_t get_metrics(ArrData* arr);
+void print_metrics(ArrData* arr);
+bool add_item(ArrData* arr, int val);
