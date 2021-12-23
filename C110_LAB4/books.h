8dbeba2c9b007721dba188bb05e1073fd1260701
zl0bster <45553832+zl0bster@users.noreply.github.com>
2021-12-23 21:21:07 +0300

    LAB 4 sort prep

--- C110_LAB4/books.h
+++ C110_LAB4/books.h
@@ -17,17 +17,6 @@ struct BookTypeList {
 	const char* descript;
 };
 
-const BookTypeList typeList[] =
-{
-	{SCI, "Science"},
-	{POE, "Poetry"},
-	{STU, "Studybook"},
-	{HUM, "Humor"},
-	{BIO, "Biology"},
-	{NOPB, "Undefined"}
-};
-
-
 typedef struct Book {
 	char author[attrSize];
 	char bookName[attrSize];
@@ -43,12 +32,15 @@ enum BookAttrs {
 };
 
 struct Book* new_book();
-int compare_books(const Book* bk1, const Book* bk2, BookAttrs attr);
 void print_book(struct Book* bk);
-char* get_book_data(Book* bk);
-void set_book_data(Book* bk, char* bkData);
 bool get_book_data_from_file(Book* bk, FILE * fl);
 void put_book_data_to_file(Book* bk, FILE* fl);
-void del_book(Book* bk);
-bool is_attr_eq(Book* bk, BookAttrs attr, ...);
 void fill_book_rnd(Book* bk);
+
+//
+//void del_book(Book* bk);
+//bool is_attr_eq(Book* bk, BookAttrs attr, ...);
+
+//int compare_books(const Book* bk1, const Book* bk2, BookAttrs attr);
+//char* get_book_data(Book* bk);
+//void set_book_data(Book* bk, char* bkData);
